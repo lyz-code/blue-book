094cb57cc74c07db955119774cd601073dce7134
lyz-code <lyz-code@users.noreply.github.com>
2021-09-04 06:35:39 +0000

    deploy: fb46211fb5757cfb74c4065abbf63b7614ca3c19

--- js/timeago_mkdocs_material.js
+++ js/timeago_mkdocs_material.js
@@ -1,17 +1,18 @@
 // Script to ensure timeago keeps working when 
 // used with mkdocs-material's instant loading feature 
 
-if (
-    typeof app !== "undefined" && 
-    typeof app.document$ !== "undefined"
-) {
-    app.document$.subscribe(function() {
+if (typeof document$ !== "undefined") {
+    document$.subscribe(function() {
         var nodes = document.querySelectorAll('.timeago');
-        var locale = nodes[0].getAttribute('locale');
-        timeago.render(nodes, locale);
+        if (nodes.length > 0) {
+          var locale = nodes[0].getAttribute('locale');
+          timeago.render(nodes, locale);
+        }
     })
 } else {
     var nodes = document.querySelectorAll('.timeago');
-    var locale = nodes[0].getAttribute('locale');
-    timeago.render(nodes, locale);
+    if (nodes.length > 0) {
+      var locale = nodes[0].getAttribute('locale');
+      timeago.render(nodes, locale);
+    }
 }
